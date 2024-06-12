41fcd60b76b8f5990c8f60fcbabe74720808a152
Jan V <janvogele2007@icloud.com>
2024-06-12 19:28:44 +0200

    Removed duplicates.

--- 22h2_fix.bat
+++ 22h2_fix.bat
@@ -4,7 +4,7 @@
     if %errorLevel% == 0 (
         bcdedit /set hypervisorlaunchtype off
         powershell.exe -ExecutionPolicy Bypass -Command "Disable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All"
-	reg add HKLM\SYSTEM\CurrentControlSet\Control\CI\Config /v VulnerableDriverBlocklistEnable /t REG_DWORD /d 0x000000
+	      reg add HKLM\SYSTEM\CurrentControlSet\Control\CI\Config /v VulnerableDriverBlocklistEnable /t REG_DWORD /d 0x000000
         
   echo Thanks to Skys#5500 for making this!
   echo Done!
