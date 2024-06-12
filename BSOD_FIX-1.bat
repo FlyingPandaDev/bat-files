fe70f32b25ddea7914a483366a1dfe93b92a182f
Jan V <janvogele2007@icloud.com>
2024-06-12 19:18:38 +0200

    first commit

---
+++ BSOD_FIX-1.bat
@@ -0,0 +1,15 @@
+@echo off
+
+NET SESSION >nul 2>&1
+IF %ERRORLEVEL% EQU 0 (
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v FeatureSettingsOverride /t REG_DWORD /d  3
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v FeatureSettingsOverrideMask /t REG_DWORD /d  3
+
+echo Thanks to Skys#5500 for making this!
+echo Please restart your PC.
+pause
+) ELSE (
+    ECHO Please run as administrator.
+    pause
+)
---
+++ testColours.bat
@@ -0,0 +1,12 @@
+@echo off
+echo.
+color 4E
+echo This is a message with a red background and yellow text.
+color 2F
+echo This is a message with a green background and white text.
+color 1E
+echo This is a message with a blue background and yellow text.
+color 07
+echo This is a normal message with default colors.
+pause
+
